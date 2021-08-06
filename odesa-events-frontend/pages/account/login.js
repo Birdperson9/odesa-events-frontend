c4ead12755ea82fc93cc7e526cd8730e8fa61d17
Yevhenii <yorieshkin@gmail.com>
2021-08-06 21:28:02 +0300

    add Auth Context

--- odesa-events-frontend/pages/account/login.js
+++ odesa-events-frontend/pages/account/login.js
@@ -4,15 +4,18 @@ import { useState, useEffect, useContext } from 'react'
 import { FaUser } from 'react-icons/fa'
 import Link from 'next/link'
 import Layout from '@/components/Layout'
+import AuthContext from '@/context/AuthContext'
 import styles from '@/styles/AuthForm.module.css'
 
 export default function LoginPage() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
+  const { login, error } = useContext(AuthContext)
+
   const handleSubmit = (e) => {
     e.preventDefault()
-    console.log({ email, password })
+    login({ email, password })
   }
 
   return (
