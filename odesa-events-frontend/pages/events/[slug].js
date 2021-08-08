29d922ccc212e8aa98310a9f861629cf1d2f5067
Yevhenii <yorieshkin@gmail.com>
2021-08-08 23:10:02 +0300

    add authentication to edit, delete, image upload

--- odesa-events-frontend/pages/events/[slug].js
+++ odesa-events-frontend/pages/events/[slug].js
@@ -1,6 +1,4 @@
-import { ToastContainer, toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
-import { FaPencilAlt, FaTimes } from 'react-icons/fa'
 import Link from 'next/link'
 import Image from 'next/image'
 import Layout from '@/components/Layout'
@@ -11,41 +9,13 @@ import { useRouter } from 'next/router'
 export default function EventPage({ evt }) {
   const router = useRouter()
 
-  const deleteEvent = async (e) => {
-    if (confirm('Are you sure?')) {
-      const res = await fetch(`${API_URL}/events/${evt.id}`, {
-        method: 'DELETE',
-      })
-
-      const data = await res.json()
-
-      if (!res.ok) {
-        toast.error(data.message)
-      } else {
-        router.push('/events')
-      }
-    }
-  }
-
   return (
     <Layout>
       <div className={styles.event}>
-        <div className={styles.controls}>
-          <Link href={`/events/edit/${evt.id}`}>
-            <a>
-              <FaPencilAlt /> Edit Event
-            </a>
-          </Link>
-          <a href='#' className={styles.delete} onClick={deleteEvent}>
-            <FaTimes /> Delete Event
-          </a>
-        </div>
-
         <span>
           {new Date(evt.date).toLocaleDateString('en-US')} at {evt.time}
         </span>
         <h1>{evt.name}</h1>
-        <ToastContainer />
         {evt.image && (
           <div className={styles.image}>
             <Image
